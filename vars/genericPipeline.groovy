a35708b3aeb2a64bc4c36afef36fc63f52b47315
Andrew Williams <andrew@Andrews-MacBook-Pro.local>
2021-01-01 12:44:15 +0000

    initial

---
+++ vars/genericPipeline.groovy
@@ -0,0 +1,49 @@
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
+    deleteDir()
+
+    pipeline {
+        agent {
+            kubernetes {
+                label 'kaniko'
+                yamlFile 'podspec.yaml'
+            }
+        }
+        stages {
+            stage('Clone') {
+	        	checkout scm
+            }
+            stage("Build/Push") {
+                environment {
+                    PATH = "/busybox:/kaniko:$PATH"
+                }
+                steps {
+                    container(name: 'kaniko', shell: '/busybox/sh') {
+                        script{
+                                sh "#!/busybox/sh /kaniko/executor -f `pwd`/Dockerfile -c `pwd` --insecure --skip-tls-verify --cache=false --destination=registry.easlab.co.uk/${config.project}/${config.name}"
+                        }
+                    }
+                }
+            }
+            stage('Deploy') {
+                environment {
+                    PROJECT = "${config.name}"
+                    NAME = "${config.name}"
+                }
+                steps {
+                    container(name: 'kube') {
+                        // Deploy to k8s cluster
+                        script {
+                            kubernetesDeploy configs: "manifests/*.yaml", kubeconfigId: 'kubeconfig'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
