f6bc5d5222c13a998f421cd84e56715dae4884f7
Andrew Williams <andrew@Andrews-MacBook-Pro.local>
2021-01-11 21:38:00 +0000

    stuff

--- vars/generateConfig.groovy
+++ vars/generateConfig.groovy
@@ -1,10 +1,8 @@
 def call (resource, namespace, hostname) {
-        switch(resource.kind) {
-
-            
-            case 'namespace': 
-            script{
-                writeFile file: "k8s/0namespace.yaml", text: """
+  switch(resource.kind) {
+    case 'namespace': 
+      script{
+        writeFile file: "k8s/0namespace.yaml", text: """
 kind: Namespace
 apiVersion: v1
 metadata:
@@ -13,13 +11,124 @@ metadata:
   name: ${namespace}
   labels:
     name: ${namespace}"""
-            }
-            break; 
+      }
+    break; 
 
 
-            case 'deployment': 
-            script{
-                writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+//     case 'deployment': 
+//       script{
+//         writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+// apiVersion: apps/v1
+// kind: Deployment
+// metadata:
+//   namespace: ${namespace}
+//   labels:
+//     app: ${resource.name}
+//   name: ${resource.name}
+// spec:
+//   replicas: 1
+//   strategy: {}
+//   selector:
+//     matchLabels:
+//       app: ${resource.name}
+//   template:
+//     metadata:
+//       labels:
+//         build_number: \$BUILD_NUMBER
+//         app: ${resource.name}
+//     spec:
+//       containers:
+//         - image: ${resource.image}
+//           name: ${resource.name}
+//           imagePullPolicy: Always
+//           resources: {}
+//           stdin: true
+//           tty: true
+// ${generateEnvYaml(resource.env)}
+//       restartPolicy: Always
+//       imagePullSecrets:
+//         - name: ${resource.imagePullSecret}"""
+//       }
+//       break; 
+
+
+      case 'registry-secret': 
+      script{
+        writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+apiVersion: v1
+data:
+  .dockerconfigjson: ${resource.dockerconfigjson}
+kind: Secret
+metadata:
+  name: ${resource.name}
+  namespace: ${namespace}
+type: kubernetes.io/dockerconfigjson"""
+      }
+      break; 
+
+
+//       case 'service': 
+//       script{
+//         writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+// apiVersion: v1
+// kind: Service
+// metadata:
+//   namespace: ${namespace}
+//   name: ${resource.name}
+// spec:
+//   ports:
+//     - name: "http"
+//       port: ${resource.port}
+//       targetPort: ${resource.port}
+//   selector:
+//     app: ${resource.target}"""
+//       }
+//       break; 
+
+
+//       case 'ingress': 
+//       script{
+//         writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+// apiVersion: v1
+// kind: Service
+// metadata:
+//   namespace: ${namespace}
+//   name: ${resource.name}
+// spec:
+//   ports:
+//     - name: "http"
+//       port: ${resource.port}
+//       targetPort: ${resource.port}
+//   selector:
+//     app: ${resource.target}
+// ---
+// apiVersion: extensions/v1beta1
+// kind: Ingress
+// metadata:
+//   annotations:
+//     cert-manager.io/cluster-issuer: letsencrypt
+//     kubernetes.io/ingress.class: nginx
+//     kubernetes.io/ingress.provider: nginx
+//   name: ${resource.name}
+//   namespace: ${namespace}
+// spec:
+//   rules:
+//   - host: ${hostname}
+//     http:
+//       paths:
+//       - backend:
+//           serviceName: ${resource.name}
+//           servicePort: ${resource.port}
+//   tls:
+//   - hosts:
+//     - ${hostname}
+//     secretName: ${resource.name}-tls-cert"""
+//       }
+//     break; 
+
+    case 'app': 
+      script{
+        writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -46,51 +155,13 @@ spec:
           resources: {}
           stdin: true
           tty: true
+${generateVolumeMountYaml(resource)}
 ${generateEnvYaml(resource.env)}
+${generateVolumeYaml(resource)}
       restartPolicy: Always
       imagePullSecrets:
-        - name: ${resource.imagePullSecret}"""
-            }
-            break; 
-
-
-            case 'registry-secret': 
-            script{
-                writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
-apiVersion: v1
-data:
-  .dockerconfigjson: ${resource.dockerconfigjson}
-kind: Secret
-metadata:
-  name: ${resource.name}
-  namespace: ${namespace}
-type: kubernetes.io/dockerconfigjson"""
-            }
-            break; 
-
-
-            case 'service': 
-            script{
-                writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
-apiVersion: v1
-kind: Service
-metadata:
-  namespace: ${namespace}
-  name: ${resource.name}
-spec:
-  ports:
-    - name: "http"
-      port: ${resource.port}
-      targetPort: ${resource.port}
-  selector:
-    app: ${resource.target}"""
-            }
-            break; 
-
-
-            case 'ingress': 
-            script{
-                writeFile file: "k8s/${resource.kind}-${resource.name}.yaml", text: """
+        - name: ${resource.imagePullSecret}
+---
 apiVersion: v1
 kind: Service
 metadata:
@@ -103,32 +174,13 @@ spec:
       targetPort: ${resource.port}
   selector:
     app: ${resource.target}
----
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  annotations:
-    cert-manager.io/cluster-issuer: letsencrypt
-    kubernetes.io/ingress.class: nginx
-    kubernetes.io/ingress.provider: nginx
-  name: ${resource.name}
-  namespace: ${namespace}
-spec:
-  rules:
-  - host: ${hostname}
-    http:
-      paths:
-      - backend:
-          serviceName: ${resource.name}
-          servicePort: ${resource.port}
-  tls:
-  - hosts:
-    - ${hostname}
-    secretName: ${resource.name}-tls-cert"""
-            }
-            break; 
-        }
-    
+${generateIngressYaml(resource)}
+${generatePVCYaml(resource)}
+"""
+      }
+      break; 
+
+  } 
 }
 
 
@@ -143,3 +195,78 @@ def generateEnvYaml (env) {
   }
   return env ? returnString : ""
 }
+
+
+def generateIngressYaml (resource) {
+  def returnString = """
+---
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  annotations:
+    cert-manager.io/cluster-issuer: letsencrypt
+    kubernetes.io/ingress.class: nginx
+    kubernetes.io/ingress.provider: nginx
+  name: ${resource.name}
+  namespace: ${resource.namespace}
+spec:
+  rules:
+  - host: ${hostname}
+    http:
+      paths:
+      - backend:
+          serviceName: ${resource.name}
+          servicePort: ${resource.port}
+  tls:
+  - hosts:
+    - ${hostname}
+    secretName: ${resource.name}-tls-cert
+"""
+  }
+  return resource.hostname ? returnString : ""
+}
+
+
+def generatePVCYaml (resource) {
+  def returnString = """
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: ${resource.name}
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 20Gi
+  storageClassName: vsphere-thin
+  volumeMode: Filesystem
+"""
+  }
+  return resource.persist ? returnString : ""
+}
+
+
+def generateVolumeYaml (resource) {
+  def returnString = """
+      volumes:
+      - name: data
+        persistentVolumeClaim:
+          claimName: ${resource.name}
+"""
+  }
+  return resource.persist ? returnString : ""
+}
+
+def generateVolumeMountYaml (resource) {
+  def returnString = """
+          volumeMounts:
+          - mountPath: \"${resource.persist}\"
+            name: data
+"""
+  }
+  return resource.persist ? returnString : ""
+}
+
+
